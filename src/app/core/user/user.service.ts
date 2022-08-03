549f892280823d31c85ef0fb2c9ed2dacef3e4a3
Jheorgenes Warlley Dias da Silva <jheorgenes@gmail.com>
2022-08-03 18:03:14 -0300

    [03-08-2022] - Implementação do Logout, utilizando o AuthGuards - Incrementando também a diretiva routerLink para manter o estado da página em SPA.

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -8,6 +8,7 @@ import * as jtw_decode from 'jwt-decode';
 export class UserService {
 
   private userSubject = new BehaviorSubject<User>(null);
+  private userName: string;
 
   constructor(private tokenService: TokenService){
     this.tokenService.hasToken() && this.decodeAndNotify();
@@ -25,6 +26,20 @@ export class UserService {
   private decodeAndNotify(){
     const token = this.tokenService.getToken(); //Pega o token do LocalStorage
     const user = jtw_decode(token) as User; //Quando decodificado, já deixa no formato da Interface User
+    this.userName = user.name; //Obtendo o userName do payload
     this.userSubject.next(user); //Faz o subscribe com os dados do usuário
+  }
+
+  logout(){
+    this.tokenService.removeToken();
+    this.userSubject.next(null); //Emitir um valor nulo para sumir o nome do usuário no header
+  }
+
+  isLogged(){
+    return this.tokenService.hasToken();
+  }
+
+  getUserName(){
+    return this.userName;
   }
 }
