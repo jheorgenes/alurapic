1b62e78e2ebd8a13e602328f98a9617abbffff89
Jheorgenes Warlley Dias da Silva <jheorgenes@gmail.com>
2022-08-05 17:59:21 -0300

    [05-08-2022]  - Criado um validador Assincrono para avaliar se já há usuário cadastrado no back-end  - Ajustes no formulário de SignUp, para efetuar o registro de novos usuários  - Aprendendo a trabalhar com AbstractControl

---
+++ src/app/home/signup/user-not-taken.validator.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { SignUpService } from './signup.service';
+import { debounceTime, switchMap, map, first } from 'rxjs/operators';;
+
+@Injectable({ providedIn: 'root' })
+export class UserNotTakenValidatorService{
+
+  constructor(private signUpService: SignUpService){}
+
+  checkUserNameTaken(){
+
+    return (control: AbstractControl) => {
+      return control
+                .valueChanges
+                .pipe(debounceTime(300)) //Evita de ficar buscando a cada letra digitada
+                .pipe(switchMap(userName => this.signUpService.checkUserNameTaken(userName))) //Consultando a api
+                .pipe(map(isTaken => isTaken ? { userNameTaken: true } : null)) //Retornando um objeto js userNameTaken=true ou null
+                .pipe(first()); //Informa que o observable completou e retorna o primeiro objeto capturado.
+    }
+  }
+}
